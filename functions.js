44778e58f638b6eb862af320a421dfa57701212a
Kedmar20 <m.kedzierski76@gmail.com>
2023-07-17 22:22:52 +0200

    updated functions.js + refactoring

--- functions.js
+++ functions.js
@@ -1,25 +1,29 @@
 export const getAllElements = () => {
    const mainContainer = document.querySelector("[data-id='a7ba009']");
-   const allPluses = document.querySelectorAll(".elementor-toggle-icon-closed");
-   let allPlusesArray = Array.from(allPluses);
-   const allMinuses = document.querySelectorAll(".elementor-toggle-icon-opened");
-   let allMinusesArray = Array.from(allMinuses);
+   const allPluses = document.querySelectorAll("span.elementor-toggle-icon-closed");
+   const allPlusesArray = Array.from(allPluses);
+   const allMinuses = document.querySelectorAll("span.elementor-toggle-icon-opened");
+   const allMinusesArray = Array.from(allMinuses);
    const allBeschreibungen = document.querySelectorAll(".elementor-tab-content");
-   let allBeschreibungenArray = Array.from(allBeschreibungen);
+   const allBeschreibungenArray = Array.from(allBeschreibungen);
 
    let isOpen = false;
+   let targetBeschreibung;
+   let targetPlus;
+   let targetMinus;
 
    const toggleDisplay = (targetToToggleBeschreibung, targetToTogglePlus, targetToToggleMinus) => {
       targetToToggleBeschreibung.style.display = isOpen ? "block" : "none";
+      targetToTogglePlus.firstElementChild.style.display = isOpen ? "none" : "block";
       targetToTogglePlus.style.display = isOpen ? "none" : "block";
+      targetToToggleMinus.firstElementChild.style.display = isOpen ? "block" : "none";
       targetToToggleMinus.style.display = isOpen ? "block" : "none";
-      targetToToggleMinus.style.height = isOpen ? "15px" : "0";
-      targetToToggleMinus.style.backgroundColor = isOpen ? "red" : "none";
 
       allPlusesArray
-         .filter((plus) => plus != targetToTogglePlus)
+         .filter((plus) => plus !== targetToTogglePlus)
          .forEach((element) => {
             element.style.display = "block";
+            targetToTogglePlus.firstElementChild.style.display = "block";
          });
 
       allMinusesArray
@@ -36,14 +40,22 @@ export const getAllElements = () => {
    };
 
    const getTarget = (e) => {
-      const targetBeschreibung = e.target.parentElement.nextElementSibling;
-      const targetPlus = e.target.parentNode.childNodes[1].firstElementChild;
-      const targetMinus = e.target.parentNode.childNodes[1].lastElementChild;
+      console.log(e.target.tagName.toLowerCase());
+      if (e.target.tagName.toLowerCase() === "a") {
+         targetBeschreibung = e.target.parentElement.nextElementSibling;
+         targetPlus = e.target.parentNode.childNodes[1].firstElementChild;
+         targetMinus = e.target.parentNode.childNodes[1].lastElementChild;
+      } else {
+         targetBeschreibung = e.target.nextElementSibling;
+         targetPlus = e.target.childNodes[1].firstElementChild;
+         targetMinus = e.target.childNodes[1].lastElementChild;
+      }
 
       if (targetBeschreibung.style.display === "block") {
-         isOpen = !isOpen;
+         isOpen = false;
       } else {
-         allBeschreibungenArray.some((el) => (el.style.display == "block" ? "" : (isOpen = !isOpen)));
+         //  allBeschreibungenArray.some((el) => (el.style.display == "block" ? "" : (isOpen = !isOpen)));
+         isOpen = true;
       }
       toggleDisplay(targetBeschreibung, targetPlus, targetMinus);
    };
